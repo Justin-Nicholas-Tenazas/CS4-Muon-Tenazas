2f95b9e1ebd46e37833c0b84d358bbe6011916e0
Justin-Nicholas-Tenazas <96160214+Justin-Nicholas-Tenazas@users.noreply.github.com>
2022-09-12 19:41:33 +0800

    Update Q2Ex2.java

--- Q1Ex2/src/q2ex2/Q2Ex2.java
+++ Q1Ex2/src/q2ex2/Q2Ex2.java
@@ -11,7 +11,7 @@ package q2ex2;
  */
 import java.util.Scanner;
 public class Q2Ex2 {
-
+	
     /**
      * @param args the command line arguments
      */
@@ -23,39 +23,63 @@ public class Q2Ex2 {
         do {
             System.out.println("Welcome to Higher or Lower! What would you want to do? Type in the number of your choice! \n[1] Start playing \n[2] Change game settings \n[3] Quit game");
             startup = sc.nextLine();
+            System.out.println("The number is set! Make your guess!");
             switch(startup) {
                 case "1": 
                     int random = (int) Math.floor(Math.random()*range) + 1;
                     for(guessCount = 2; guessCount >= 0; guessCount--){
-                        System.out.println("The number is set! Make your guess!");
                         String guess = sc.nextLine();
                         int trial = Integer.parseInt(guess);
-                        if(random < trial){System.out.printf("Sorry, guess lower! you have %s guesses left! Go again! \n", guessCount);}
-                        else if(random > trial){System.out.printf("Sorry, guess higher! you have %s guesses left! Go again! \n", guessCount);}
+                        if(random < trial){
+                            if(guessCount == 0) {
+                                System.out.println("You lost... Play again? y/n");
+                                String reviveLow = sc.nextLine();
+                                if(reviveLow == "n") {
+                                System.out.println("See you next time!");
+                                return;
+                                }
+                                else if (reviveLow == "y") {break;}
+                                
+                            }
+                            else{System.out.printf("Sorry, guess higher! you have %s guesses left! Go again! \n", guessCount);}
+                            
+                        }
+                        else if(random > trial){
+                            if(guessCount == 0) {
+                                System.out.println("You lost... Play again? y/n");
+                                String reviveHigh = sc.nextLine();
+                                if(reviveHigh == "n") {
+                                System.out.println("See you next time!");
+                                return;
+                                }
+                                else if (reviveHigh == "y") {break;}
+                                
+                            }
+                            else{System.out.printf("Sorry, guess higher! you have %s guesses left! Go again! \n", guessCount);}
+                        }
                         else if(random == trial) {System.out.println("Congratulations! You got it! Play again? y/n");
                             String confirm = sc.nextLine();
-                            switch(confirm) {
-                                case "y":
+                            if(confirm == "n") {
+                                System.out.println("See you next time!");
+                                return;
                             }
-                        }
-                        else{
-                            if(guessCount == 0) {System.out.println("You lost... Play again?");}
+                            else if (confirm == "y") {break;}
                         }
                     }
-                    
-                    break;
-            
+                break;
+                
                 case "2": 
                     System.out.println("How many guesses would you like to use?");
                     break;
             
                 case "3": System.out.println("See you next time!");
                     break;
-        
             }
-        } 
-        while(!startup.equals("3"));
+                    
         
-    }
-    
+        } while(!startup.equals("3"));
+    } 
+        
+        
 }
+    
