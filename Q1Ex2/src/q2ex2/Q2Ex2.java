1094ab083d34dc93784c2484125620cb0d1487e3
Justin-Nicholas-Tenazas <96160214+Justin-Nicholas-Tenazas@users.noreply.github.com>
2022-09-15 11:26:19 +0800

    Update Q2Ex2.java

--- Q1Ex2/src/q2ex2/Q2Ex2.java
+++ Q1Ex2/src/q2ex2/Q2Ex2.java
@@ -19,65 +19,68 @@ public class Q2Ex2 {
         Scanner sc = new Scanner(System.in);
         String startup;
         int range = 5;
-        int guessCount = 2;
+        int defaultGuess = 2;
+        int guessCount;
         do {
             System.out.println("Welcome to Higher or Lower! What would you want to do? Type in the number of your choice! \n[1] Start playing \n[2] Change game settings \n[3] Quit game");
+            String revive = "y";
             startup = sc.nextLine();
             switch(startup) {
                 case "1": 
                     int random = (int) Math.floor(Math.random()*range) + 1;
                     System.out.println("The number is set! Make your guess!");
-                    for(guessCount = guessCount; guessCount >= 0; guessCount--){
-                        String guess = sc.nextLine();
-                        int trial = Integer.parseInt(guess);
-                        if(random < trial){
-                            if(guessCount == 0) {
-                                System.out.println("You lost... Play again? y/n");
-                                String reviveLow = sc.nextLine();
-                                if(reviveLow == "n") {
-                                System.out.println("See you next time!");
-                                return;
+                    while(revive.equals("y")) {
+                        for(guessCount = defaultGuess; guessCount >= 0; guessCount--){
+                            int guess = sc.nextInt();
+                            int trial = guess;
+                            if(random > trial){
+                                if(guessCount == 0) {
+                                    System.out.println("You lost... Play again? y/n");
+                                    revive = sc.nextLine();
+                                    if(revive.equals("n")) {
+                                    System.out.println("See you next time!");
+                                    return;
+                                    }
+                                    else if(revive.equals("y")) {break;}
                                 }
-                                else if (reviveLow == "y") {break;}
-                                
+                                else{System.out.printf("Sorry, guess higher! you have %s guesses left! Go again! \n", guessCount);}                        
                             }
-                            else{System.out.printf("Sorry, guess higher! you have %s guesses left! Go again! \n", guessCount);}
-                            
-                        }
-                        else if(random > trial){
-                            if(guessCount == 0) {
-                                System.out.println("You lost... Play again? y/n");
-                                String reviveHigh = sc.nextLine();
-                                if(reviveHigh == "n") {
-                                System.out.println("See you next time!");
-                                
+                            else if(random < trial){
+                                if(guessCount == 0) {
+                                    System.out.println("You lost... Play again? y/n");
+                                    revive = sc.nextLine();
+                                    if(revive.equals("n")) {
+                                    System.out.println("See you next time!");
+                                    return;
+                                    }
+                                    else if(revive.equals("y")) {break;}
                                 }
-                                else if (reviveHigh == "y") {break;}
-                                
+                                else{System.out.printf("Sorry, guess lower! you have %s guesses left! Go again! \n", guessCount);}
                             }
-                            else{System.out.printf("Sorry, guess higher! you have %s guesses left! Go again! \n", guessCount);}
-                        }
-                        else if(random == trial) {System.out.println("Congratulations! You got it! Play again? y/n");
-                            String confirm = sc.nextLine();
-                            if(confirm == "n") {
-                                System.out.println("See you next time!");
-                                
+                            else if(random == trial) {
+                                System.out.println("Congratulations! You got it! Play again? y/n");
+                                revive = sc.nextLine();
+                                if(revive.equals("n")) {
+                                    System.out.println("See you next time!");
+                                    return;
+                                }
+                                else if(revive.equals("y")) {break;}
                             }
-                            else if (confirm == "y") {break;}
                         }
                     }
                 break;
                 
                 case "2": 
                     System.out.println("How many guesses would you like to use?");
-                    String newCount = sc.nextLine();
-                    guessCount = Integer.parseInt(newCount);
-                    guessCount -= 1;
+                    defaultGuess = sc.nextInt();
+                    guessCount = defaultGuess;
+                    defaultGuess -= 1;
                     System.out.println("What will be the highest possible number you can guess?");
-                    String newrange = sc.nextLine();
-                    range = Integer.parseInt(newrange);
+                    int newrange = sc.nextInt();
+                    range = newrange;
                     range -= 1;
                     break;
+
             
                 case "3": 
                     System.out.println("See you next time!");
