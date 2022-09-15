daab0e8be08b6d31167e786c63ac02e548acda01
Justin-Nicholas-Tenazas <96160214+Justin-Nicholas-Tenazas@users.noreply.github.com>
2022-09-15 20:57:54 +0800

    Update Q2Ex2.java

--- Q1Ex2/src/q2ex2/Q2Ex2.java
+++ Q1Ex2/src/q2ex2/Q2Ex2.java
@@ -23,52 +23,25 @@ public class Q2Ex2 {
         int guessCount;
         do {
             System.out.println("Welcome to Higher or Lower! What would you want to do? Type in the number of your choice! \n[1] Start playing \n[2] Change game settings \n[3] Quit game");
-            String revive = "y";
             startup = sc.nextLine();
             switch(startup) {
                 case "1": 
                     int random = (int) Math.floor(Math.random()*range) + 1;
                     System.out.println("The number is set! Make your guess!");
-                    while(revive.equals("y")) {
-                        for(guessCount = defaultGuess; guessCount >= 0; guessCount--){
-                            int guess = sc.nextInt();
-                            int trial = guess;
-                            if(random > trial){
-                                if(guessCount == 0) {
-                                    System.out.println("You lost... Play again? y/n");
-                                    revive = sc.nextLine();
-                                    if(revive.equals("n")) {
-                                    System.out.println("See you next time!");
-                                    return;
-                                    }
-                                    else if(revive.equals("y")) {break;}
-                                }
-                                else{System.out.printf("Sorry, guess higher! you have %s guesses left! Go again! \n", guessCount);}                        
-                            }
-                            else if(random < trial){
-                                if(guessCount == 0) {
-                                    System.out.println("You lost... Play again? y/n");
-                                    revive = sc.nextLine();
-                                    if(revive.equals("n")) {
-                                    System.out.println("See you next time!");
-                                    return;
-                                    }
-                                    else if(revive.equals("y")) {break;}
-                                }
-                                else{System.out.printf("Sorry, guess lower! you have %s guesses left! Go again! \n", guessCount);}
-                            }
-                            else if(random == trial) {
-                                System.out.println("Congratulations! You got it! Play again? y/n");
-                                revive = sc.nextLine();
-                                if(revive.equals("n")) {
-                                    System.out.println("See you next time!");
-                                    return;
-                                }
-                                else if(revive.equals("y")) {break;}
-                            }
+                    for(guessCount = defaultGuess; guessCount >= 0; guessCount--){
+                        String guess = sc.nextLine();
+                        int trial = Integer.parseInt(guess);
+                        if(random > trial){
+                            if(guessCount == 0) {System.out.println("You lost...");}
+                            else{System.out.printf("Sorry, guess higher! you have %s guesses left! Go again! \n", guessCount);}                        
                         }
+                        else if(random < trial) {
+                            if(guessCount == 0) {System.out.println("You lost...");}
+                            else{System.out.printf("Sorry, guess lower! you have %s guesses left! Go again! \n", guessCount);}
+                        }
+                        else if(random == trial) {System.out.println("Congratulations! You got it!");}
                     }
-                break;
+                    break;
                 
                 case "2": 
                     System.out.println("How many guesses would you like to use?");
@@ -80,7 +53,6 @@ public class Q2Ex2 {
                     range = newrange;
                     range -= 1;
                     break;
-
             
                 case "3": 
                     System.out.println("See you next time!");
